5e08671deff591fbf7a5b9e30af2225a7a0b30f4
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-01-31 12:02:37 +0000

    Add new item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+
+    //declare new state variables
+    const [value, setValue] = useState('');
+    const [toDos, setToDos] = useState([]); // keep track of to-do list
+
+    const handleChange = event => {setValue(event.target.value)}
+    const handleSubmit = event => {setToDos([...toDos, value])} // create array and add new value
+    console.log("toDos value: ", toDos)
+
+    return (
+
+      <div className="container">
+
+        <h1> To do list </h1>
+
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" value={value} onChange={handleChange} />
+          <div className="input-group-append">
+              <button className="btn btn-outline-secondary" type="button" onClick={handleSubmit}>Add</button>
+          </div>
+        </div>
+
+        <hr />
+
+      </div>
   );
 }
 
